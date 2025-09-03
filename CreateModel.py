8fdae0eb420dec3b25e339461ba366b7f2eecc81
jassow0208 <jasinsowandi@hotmail.com>
2025-09-03 11:31:35 +0700

    Initial commit

---
+++ CreateModel.py
@@ -0,0 +1,51 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from category_encoders import BinaryEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import pickle
+
+
+# Contoh dataset
+data = {
+    'kategori_produk': ['Elektronik', 'Pakaian', 'Makanan', 'Elektronik', 'Pakaian', 'Makanan'],
+    'harga_produk': [500, 300, 200, 400, 250, 150],
+    'penjualan': [100, 80, 120, 110, 90, 130]
+}
+
+# Konversi ke DataFrame
+df = pd.DataFrame(data)
+
+# Pisahkan fitur dan target
+X = df[['kategori_produk', 'harga_produk']] 
+y = df['penjualan']
+
+# Inisialisasi BinaryEncoder
+binary_encoder = BinaryEncoder(cols=['kategori_produk'])
+
+# Transformasi data
+X_encoded = binary_encoder.fit_transform(X)
+
+print(X_encoded)
+
+# Bagi data menjadi training set dan test set
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=42)
+
+# Inisialisasi model regresi linear
+model = LinearRegression()  
+# Latih model
+model.fit(X_train, y_train)
+# Prediksi pada test set
+y_pred = model.predict(X_test)
+# Evaluasi model
+mse = mean_squared_error(y_test, y_pred)    
+r2 = r2_score(y_test, y_pred)
+
+print(f'Mean Squared Error: {mse}')
+print(f'R^2 Score: {r2}')
+
+# Simpan model ke file .pkl
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+
+# Muat model dari file .pkl
+with open('model.pkl', 'rb') as file:
+    loaded_model = pickle.load(file)
+
+# Judul halaman
+st.title("Prediksi Penjualan Produk")
+
+# Teks sederhana
+st.write("Ini adalah aplikasi untuk memprediksi penjualan produk menggunakan model regresi linear.")
+
+# Input teks dari pengguna
+number_input = st.number_input("Harga Produk:")
+jabatan_input = st.number_input("Jabatan (encoded):")
+user_input = st.text_input("Masukkan nama Anda:")
+
+# Contoh data baru
+new_data = [[ 1,1,number_input]]  # Misalnya: harga_produk = 500, jabatan_encoded = 3
+
+# Menampilkan pesan sesuai input
+if user_input:
+    st.write(f"Halo, {user_input}! Selamat datang di Streamlit.")
+
+# Tombol interaktif
+if st.button("Prediksi"):
+    # Lakukan prediksi menggunakan model yang dimuat
+    prediction = loaded_model.predict(new_data)
+    st.write(f"Prediksi Penjualan:{prediction}")
